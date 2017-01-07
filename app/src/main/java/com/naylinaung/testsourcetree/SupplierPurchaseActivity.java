75e5ddde67ab1edfb18f456c4687e43ea61c514d
SteveNay <naylinaung.softwarearchitect@gmail.com>
2017-01-07 19:10:38 +0630

    Added SupplierPurcahse Activity

---
+++ app/src/main/java/com/naylinaung/testsourcetree/SupplierPurchaseActivity.java
@@ -0,0 +1,13 @@
+package com.naylinaung.testsourcetree;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SupplierPurchaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supplier_purchase);
+    }
+}
